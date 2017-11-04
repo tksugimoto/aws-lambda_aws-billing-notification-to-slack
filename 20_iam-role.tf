158584b3f9d1a11a63597bba67bf0bdafd3e888f
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-11-04 18:32:06 +0900

    [Clean] Lambda用policyをインラインポリシーからAWS管理ポリシーに変更

--- 20_iam-role.tf
+++ 20_iam-role.tf
@@ -4,14 +4,12 @@ resource "aws_iam_role" "iam_for_lambda" {
 	assume_role_policy = "${file("./iam-role/assume_role_policy-trust_lambda.json")}"
 }
 
-resource "aws_iam_role_policy" "aws_lambda_basic_execution" {
-	name = "aws_lambda_basic_execution"
-	role = "${aws_iam_role.iam_for_lambda.id}"
-	policy = "${file("./iam-role/policy-aws_lambda_basic_execution.json")}"
+resource "aws_iam_role_policy_attachment" "aws_lambda_basic_execution_attach" {
+	role = "${aws_iam_role.iam_for_lambda.name}"
+	policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_role_policy" "amazon_s3_read_only_access" {
-	name = "amazon_s3_read_only_access"
-	role = "${aws_iam_role.iam_for_lambda.id}"
-	policy = "${file("./iam-role/policy-amazon_s3_read_only_access.json")}"
+resource "aws_iam_role_policy_attachment" "amazon_s3_read_only_access_attach" {
+	role = "${aws_iam_role.iam_for_lambda.name}"
+	policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
