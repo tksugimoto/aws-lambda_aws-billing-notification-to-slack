6029d8da5e0d52c948e8d4bfa74bea7930e6544d
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-11-04 18:33:42 +0900

    [Move] assume_role_policyをフォルダに移動
    ファイル名のprefixからフォルダ名に変更

--- 20_iam-role.tf
+++ 20_iam-role.tf
@@ -1,7 +1,7 @@
 
 resource "aws_iam_role" "iam_for_lambda" {
 	name = "${var.prefix}-iam_for_lambda"
-	assume_role_policy = "${file("./iam-role/assume_role_policy-trust_lambda.json")}"
+	assume_role_policy = "${file("./iam-role/assume_role_policy/trust_lambda.json")}"
 }
 
 resource "aws_iam_role_policy_attachment" "aws_lambda_basic_execution_attach" {
