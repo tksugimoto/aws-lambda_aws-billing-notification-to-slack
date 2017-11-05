e8710d4a7c4ae585f8bcf4e929e61be6f922d672
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-11-05 21:32:34 +0900

    [Update] AWS Lambdaの runtime を nodejs 6.10 に :up:

--- 30_lambda-function.tf
+++ 30_lambda-function.tf
@@ -7,7 +7,7 @@ data "archive_file" "source_code" {
 resource "aws_lambda_function" "notification" {
 	function_name = "${var.prefix}"
 	role = "${aws_iam_role.iam_for_lambda.arn}"
-	runtime = "nodejs4.3"
+	runtime = "nodejs6.10"
 	handler = "index.handler"
 	timeout = 10
 	filename = "./dist/index.zip"
