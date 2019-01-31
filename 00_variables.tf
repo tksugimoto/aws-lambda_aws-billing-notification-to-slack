2f4e334692b1216f96172c022c1128b8a2b5ea30
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-01-31 23:25:07 +0900

    refactor: .tfファイルをformat
    terraform fmt コマンドを使用

--- 00_variables.tf
+++ 00_variables.tf
@@ -1,5 +1,6 @@
 # AWS APIキー変数設定
 variable "aws_access_key" {}
+
 variable "aws_secret_key" {}
 
 # 名前のPrefix
@@ -7,7 +8,6 @@ variable "prefix" {}
 
 # リージョン
 variable "region" {}
-
 
 # 請求レポートを保存しているバケット名
 variable "bucket" {}
@@ -19,19 +19,19 @@ variable "slack_webhook_url" {}
 # format: "#channel-name"
 # デフォルト（空文字）はWebhookのデフォルト値
 variable "channel" {
-	default = ""
+  default = ""
 }
 
 # Slack 通知のユーザー表示
 # デフォルト（空文字）はWebhookのデフォルト値
 # 既知の問題: 半角スペース以降は無視
 variable "username" {
-	default = ""
+  default = ""
 }
 
 # Slack 通知のアイコン
 # format: ":icon_name:"
 # デフォルト（空文字）はWebhookのデフォルト値
 variable "icon_emoji" {
-	default = ""
+  default = ""
 }
--- 30_lambda-function.tf
+++ 30_lambda-function.tf
@@ -5,20 +5,21 @@ data "archive_file" "source_code" {
 }
 
 resource "aws_lambda_function" "notification" {
-	function_name = "${var.prefix}"
-	role = "${aws_iam_role.iam_for_lambda.arn}"
-	runtime = "nodejs6.10"
-	handler = "index.handler"
-	timeout = 10
-	filename = "./dist/index.zip"
-	source_code_hash = "${data.archive_file.source_code.output_base64sha256}"
-    environment {
-        variables = {
-            bucket            = "${var.bucket}"
-            slack_webhook_url = "${var.slack_webhook_url}"
-            channel           = "${var.channel}"
-            username          = "${var.username}"
-            icon_emoji        = "${var.icon_emoji}"
-        }
+  function_name    = "${var.prefix}"
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  runtime          = "nodejs6.10"
+  handler          = "index.handler"
+  timeout          = 10
+  filename         = "./dist/index.zip"
+  source_code_hash = "${data.archive_file.source_code.output_base64sha256}"
+
+  environment {
+    variables = {
+      bucket            = "${var.bucket}"
+      slack_webhook_url = "${var.slack_webhook_url}"
+      channel           = "${var.channel}"
+      username          = "${var.username}"
+      icon_emoji        = "${var.icon_emoji}"
     }
+  }
 }
