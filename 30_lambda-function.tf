d6f89e352f3c67c51d6a0f03bdbb4a2ba4ceb98a
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-04-27 22:26:10 +0900

    feat: AWS Lambdaの runtime を nodejs 8.10 に :up:

--- 30_lambda-function.tf
+++ 30_lambda-function.tf
@@ -7,7 +7,7 @@ data "archive_file" "source_code" {
 resource "aws_lambda_function" "notification" {
   function_name    = "${var.prefix}"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  runtime          = "nodejs6.10"
+  runtime          = "nodejs8.10"
   handler          = "index.handler"
   timeout          = 10
   filename         = "${data.archive_file.source_code.output_path}"
